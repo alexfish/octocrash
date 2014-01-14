eea13d5de95b485f6a0a7ef02b3bf2aa1cf8fd9b
Alex Fish <fish@ustwo.co.uk>
2014-01-14 13:34:24 +0000

    Adding a PLCrashReport category

---
+++ Classes/PLCrashReport+AEFIssue.h
@@ -0,0 +1,18 @@
+//
+//  PLCrashReport+AEFIssue.h
+//  OctoCrash
+//
+//  Created by Alex Fish on 1/14/14.
+//  Copyright (c) 2014 alexefish. All rights reserved.
+//
+
+#import <CrashReporter/CrashReporter.h>
+
+/**
+ A catergory on PLCrashReport to provide some additional
+ functionality to handle the repoting of PLCrashReport objects
+ as github issues
+ */
+@interface PLCrashReport (AEFIssue)
+
+@end
---
+++ Classes/PLCrashReport+AEFIssue.m
@@ -0,0 +1,13 @@
+//
+//  PLCrashReport+AEFIssue.m
+//  OctoCrash
+//
+//  Created by Alex Fish on 1/14/14.
+//  Copyright (c) 2014 alexefish. All rights reserved.
+//
+
+#import "PLCrashReport+AEFIssue.h"
+
+@implementation PLCrashReport (AEFIssue)
+
+@end
