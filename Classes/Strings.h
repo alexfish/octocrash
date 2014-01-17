60263b7ad81395d1824ada78244abd8843e47e2e
Alex Fish <alex@alexefish.com>
2014-01-17 21:09:33 +0000

    Adding AEFLocalizable files
    * Adding a new strings header for localizable loading

---
+++ Classes/Strings.h
@@ -0,0 +1,10 @@
+//
+//  Strings.h
+//  OctoCrash
+//
+//  Created by Alex Fish on 17/01/2014.
+//  Copyright (c) 2014 alexefish. All rights reserved.
+//
+
+
+#define AEFLocalizedString(x, y) NSLocalizedStringFromTable((x), @"AEFLocalizable", (y))
