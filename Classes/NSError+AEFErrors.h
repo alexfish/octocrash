dfd24af01570acfb0d3dd2b35abe7ccd2b3b5f72
Alex Fish <alex@alexefish.com>
2014-01-18 14:44:05 +0000

    Adding NSError category for easy error creation

--- Classes/NSError+AEFErrors.h
+++ Classes/NSError+AEFErrors.h
@@ -8,6 +8,34 @@
 
 #import <Foundation/Foundation.h>
 
+
+/**
+ *  An Enum of available AEFErrorCodes
+ */
+NS_ENUM(NSInteger, AEFErrorCode)
+{
+    /** A generic error code when something is not found */
+    AEFErrorCodeNotFound = 800,
+};
+
+/**
+ *  The AEFErrorDomain name
+ */
+static NSString *const kAEFErrorDomain = @"AEFError";
+
+
+/**
+ *  A category to provide OctoCrash specific error functionality
+ */
 @interface NSError (AEFErrors)
+
+/**
+ *  Return an NSError object for a specific error code
+ *
+ *  @param errorCode The error code to generate an error for
+ *
+ *  @return An new NSError instance for the given error code
+ */
++ (NSError *)errorWithCode:(NSInteger)errorCode;
 
 @end
--- Classes/NSError+AEFErrors.m
+++ Classes/NSError+AEFErrors.m
@@ -8,6 +8,48 @@
 
 #import "NSError+AEFErrors.h"
 
+
 @implementation NSError (AEFErrors)
+
+
+#pragma mark - Errors
+
++ (NSError *)errorWithCode:(NSInteger)errorCode
+{
+    NSError *error = nil;
+    
+    switch (errorCode) {
+        case AEFErrorCodeNotFound:
+            error = [NSError notFoundError];
+            break;
+        default:
+            error = [NSError genericError];
+            break;
+    }
+    return error;
+}
+
+
+#pragma mark - Errors (Private)
+
++ (NSError *)notFoundError
+{
+	NSDictionary *userInfo = @{
+                               NSLocalizedDescriptionKey: AEFLocalizedString(@"ERROR_DESCRIPTION_NOT_FOUND", nil),
+                               NSLocalizedFailureReasonErrorKey: AEFLocalizedString(@"ERROR_REASON_NOT_FOUND", nil),
+                               };
+    
+	return [NSError errorWithDomain:kAEFErrorDomain code:AEFErrorCodeNotFound userInfo:userInfo];
+}
+
++ (NSError *)genericError
+{
+	NSDictionary *userInfo = @{
+                               NSLocalizedDescriptionKey: AEFLocalizedString(@"ERROR_DESCRIPTION_GENERIC", @""),
+                               NSLocalizedFailureReasonErrorKey: AEFLocalizedString(@"ERROR_REASON_GENERIC", nil),
+                               };
+    
+	return [NSError errorWithDomain:kAEFErrorDomain code:AEFErrorCodeNotFound userInfo:userInfo];
+}
 
 @end
