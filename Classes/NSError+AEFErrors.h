3207fb9c4ab7803c85258f03e50863949255d838
Alex Fish <alex@alexefish.com>
2014-01-18 14:35:02 +0000

    Renaming Strings to AEFStrings

---
+++ Classes/NSError+AEFErrors.h
@@ -0,0 +1,13 @@
+//
+//  NSError+AEFErrors.h
+//  OctoCrash
+//
+//  Created by Alex Fish on 18/01/2014.
+//  Copyright (c) 2014 alexefish. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSError (AEFErrors)
+
+@end
---
+++ Classes/NSError+AEFErrors.m
@@ -0,0 +1,13 @@
+//
+//  NSError+AEFErrors.m
+//  OctoCrash
+//
+//  Created by Alex Fish on 18/01/2014.
+//  Copyright (c) 2014 alexefish. All rights reserved.
+//
+
+#import "NSError+AEFErrors.h"
+
+@implementation NSError (AEFErrors)
+
+@end
